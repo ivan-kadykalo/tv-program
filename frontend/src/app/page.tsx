b25aae5bca84f933411baa37dad8989e9ac813ce
Ivan Kadykalo <vanya.k@mate.academy>
2024-09-21 20:42:06 +0300

    add next app

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,45 +1,95 @@
-"use client";
-
-import styles from "./style/page.module.scss";
-import React, {useCallback, useMemo, useState} from "react";
-import {EventsList} from "@/app/components/EventsList/EventsList";
-import {ProgramType, TVEvent} from "@/app/components/Event/Event.typedefs";
-import {EventTypesSection} from "@/app/components/EventTypesSection/EventTypesSection";
-
-
-import tvSchedule from '../../../server/src/tv_schedule.json';
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const [selectedType, setSelectedType] = useState<ProgramType>(ProgramType.MOVIE);
-
-  const filteredEvents = useMemo(() => (
-    tvSchedule.filter((event) => event.type === selectedType)
-  ), [selectedType]);
-
-  const handleTypeSelect = useCallback((type: ProgramType) => {
-    setSelectedType(type);
-  }, []);
-
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Що сьогодні подивитись?
-        </h1>
-
-        <p className={styles.description}>
-          Глянь телепрограму на тиждень
-        </p>
-
-        <EventTypesSection
-          selectedType={selectedType}
-          onTypeSelect={handleTypeSelect}
+        <Image
+          className={styles.logo}
+          src="https://nextjs.org/icons/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
         />
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-        <EventsList
-          events={filteredEvents}
-        />
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className={styles.logo}
+              src="https://nextjs.org/icons/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
       </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
     </div>
   );
 }
