e7d6b0bb16ccabd09d2e29817d7761428a178ba6
Ivan Kadykalo <vanya.k@mate.academy>
2024-09-25 23:16:10 +0300

    build files

---
+++ api/dist/controllers/TVScraper/TVScraper.constants.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WRITE_FILE_DIRECTORY = exports.PERIOD = exports.BASE_URL = void 0;
+exports.BASE_URL = 'https://teleprograma.com.ua/cat/main/date/';
+exports.PERIOD = '/period/allday';
+exports.WRITE_FILE_DIRECTORY = './data/tv_schedule.json';
