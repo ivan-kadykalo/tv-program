9cba79ed02270e214bd9356cdae3025b35838c49
Ivan Kadykalo <vanya.k@mate.academy>
2024-10-01 15:58:31 +0300

    create api route

--- frontend/src/app/api/events.ts
+++ frontend/src/app/api/events.ts
@@ -1,21 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Pool } from 'pg';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  // connectionString: "postgres://default:BSc1IVYWPMb9@ep-little-block-a2fbpc7j-pooler.eu-central-1.aws.neon.tech/verceldb?sslmode=require",
-});
+import { sql } from '@vercel/postgres';
+const TABLE_NAME = 'events';
 
 const queryEvents = async () => {
-  const client = await pool.connect();
   try {
-    const res = await client.query('SELECT * FROM your_table_name'); // Replace with your actual table name
+    const res = await sql`SELECT * FROM ${TABLE_NAME}`;
+
     return res.rows;
   } catch (error) {
     console.error('Error querying events:', error);
     throw new Error('Database query failed');
-  } finally {
-    client.release();
   }
 };
 
