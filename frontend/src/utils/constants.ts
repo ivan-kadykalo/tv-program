c978ff12a74fedb959b6f0f99748beb8dc481b4e
Ivan Kadykalo <vanya.k@mate.academy>
2024-11-13 10:55:41 +0200

    fix title map

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -4,9 +4,9 @@ export const QUERY_TYPE = 'type';
 export const SEARCH_LINK_BASE = 'https://www.google.com/search';
 
 export const MAP_PAGE_TITLE: Record<ProgramType, string> = {
-  [ProgramType.ALL]: 'Фільми',
-  [ProgramType.MOVIE]: 'Мультфільми',
-  [ProgramType.CARTOON]: 'Всі програми',
+  [ProgramType.ALL]: 'Всі програми',
+  [ProgramType.MOVIE]: 'Фільми',
+  [ProgramType.CARTOON]: 'Мультфільми',
 };
 
 export const MAP_BUTTON_NAME: Record<ProgramType, string> = {
