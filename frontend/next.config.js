9f92ac744c14e04f97d89587721e38a234bbbbb1
Ivan Kadykalo <vanya.k@mate.academy>
2024-11-21 10:48:46 +0200

    fix config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,17 +6,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
+  ...withPWA({
+    pwa: {
+      dest: 'public',
+      disable: process.env.NEXT_PUBLIC_ENV === 'development',
+    },
+  }),
 };
-
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NEXT_PUBLIC_ENV === 'development',
-  },
-});
