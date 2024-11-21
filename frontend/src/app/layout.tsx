6ba47776f092f062aa542a724c33e8210d323857
Ivan Kadykalo <vanya.k@mate.academy>
2024-11-21 17:16:41 +0200

    remove comments

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,13 +17,8 @@ export default function RootLayout({
   return (
     <html lang="uk-UA">
     <head>
-      {/*<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>*/}
-      {/*<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>*/}
-      {/*<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>*/}
-      {/*<link rel="manifest" href="/manifest.json"/>*/}
-
       <title>
-        TV program
+        Телепрограма
       </title>
 
       <meta
